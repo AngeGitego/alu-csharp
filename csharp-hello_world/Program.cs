57b1d1becaaa0d3951e818deaddee86ddf97773f
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-01-10 15:05:10 +0200

    Update Program.cs

--- csharp-hello_world/Program.cs
+++ csharp-hello_world/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("The template \"Console Application\" was created successfully.");
+Console.WriteLine("Hello, World!");
