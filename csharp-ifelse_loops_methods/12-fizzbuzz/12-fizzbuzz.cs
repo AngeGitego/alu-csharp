b17ad3bd87311306335e05960c442ec99d885c88
AngeGitego <k.gitego@alustudent.com>
2025-01-13 13:04:54 +0200

    chnages

--- csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -1,36 +1,22 @@
-﻿using System;
+using System;
 
-class FizzBuzz
+class Program
 {
-    public static void Main()
+    static void Main()
     {
         for (int i = 1; i <= 100; i++)
         {
-            if (i % 3 == 0 && i % 5 == 0)
-            {
+            if (i % 15 == 0)
                 Console.Write("FizzBuzz");
-            }
             else if (i % 3 == 0)
-            {
                 Console.Write("Fizz");
-            }
             else if (i % 5 == 0)
-            {
                 Console.Write("Buzz");
-            }
             else
-            {
                 Console.Write(i);
-            }
 
-            // Add a space after each number except the last one
             if (i < 100)
-            {
-                Console.Write(" ");
-            }
+                Console.Write(" "); // No newline or \r\n
         }
-
-        // End with a newline
-        Console.WriteLine();
     }
 }
