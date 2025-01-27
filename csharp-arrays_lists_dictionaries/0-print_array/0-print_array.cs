34cf7531693f750b17741e6429c80f06a7954fc7
AngeGitego <k.gitego@alustudent.com>
2025-01-27 12:13:20 +0200

    new files

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        // Handle size less than 0
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        // Handle size equal to 0
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+
+        // Create and populate the array
+        int[] newArray = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+        }
+
+        // Print the array
+        Console.WriteLine(string.Join(" ", newArray));
+
+        // Return the new array
+        return newArray;
+    }
+}
