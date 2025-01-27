0e18e386850ef4c42d27c53543eb67945fe6bd5f
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-01-27 16:07:12 +0200

    Update 7-islower.cs

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -1,10 +1,14 @@
 using System;
 
-class Character
+public class Character
 {
     public static bool IsLower(char c)
     {
-        // Check if the character is between 'a' and 'z' in ASCII range
-        return c >= 'a' && c <= 'z';
+        // Check if the character is between 'a' and 'z'
+        if (c >= 'a' && c <= 'z')
+        {
+            return true;
+        }
+        return false;
     }
 }
