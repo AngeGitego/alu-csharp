5de2a127c8e4928821d89c76cefc0fa339394420
AngeGitego <k.gitego@alustudent.com>
2025-01-27 12:48:19 +0200

    add new files

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] array2 = null;
+        int[] array3 = { };
+        int[] array4 = { 98, -10, 0, 972, -42 };
+
+        Array.Reverse(array1); // Reverse and print array1
+        Array.Reverse(array2); // Reverse and print array2 (null)
+        Array.Reverse(array3); // Reverse and print array3 (empty)
+        Array.Reverse(array4); // Reverse and print array4
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine(); // Print an empty line for null or empty array
+            return;
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i]);
+            if (i > 0)
+                Console.Write(" "); // Add a space between elements
+        }
+        Console.WriteLine(); // End the line after printing the array
+    }
+}
