5672384f18c6635dea5ebc66a9ec237d3fb1b344
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-01-27 15:46:26 +0200

    Update 12-main.cs

--- csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-main.cs
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-main.cs
@@ -1,21 +1,18 @@
 using System;
 using System.Collections.Generic;
 
-namespace PrintSortedDictionaryApp
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Dictionary<string, string> myDict = new Dictionary<string, string>();
+        Dictionary<string, string> myDict = new Dictionary<string, string>();
 
-            myDict.Add("language", "C");
-            myDict.Add("track", "low level");
-            myDict.Add("school", "Holberton");
-            myDict.Add("address", "972 Mission St.");
-            myDict.Add("city", "San Francisco");
+        myDict.Add("language", "C");
+        myDict.Add("track", "low level");
+        myDict.Add("school", "Holberton");
+        myDict.Add("address", "972 Mission St.");
+        myDict.Add("city", "San Francisco");
 
-            Dictionary.PrintSorted(myDict);
-        }
+        Dictionary.PrintSorted(myDict);
     }
 }
