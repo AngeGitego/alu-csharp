6eb892b77ffe8250d44c357708017289a7369f86
AngeGitego <k.gitego@alustudent.com>
2025-01-30 13:58:21 +0200

    changes on task 5

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -7,10 +7,14 @@ class Program
         // Loop to print all numbers from 0 to 99 in the desired format
         for (int i = 0; i < 100; i++)
         {
-            if (i < 99)
-                Console.Write($"{i:D2}, ");
-            else
-                Console.WriteLine($"{i:D2}"); // Print the last number with a newline
+            if (i==99){
+                Console.Write($"{i}\n");
+            }
+            else{
+                Console.Write($"{i.ToString("D2")}, ");
+            }
         }
     }
+
 }
+
