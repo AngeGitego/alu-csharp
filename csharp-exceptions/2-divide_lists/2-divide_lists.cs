20a2f39a65e0cc6449a13d4f8eaf471adff04048
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-02-15 19:42:55 +0200

    Update 2-divide_lists.cs

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,36 +1,47 @@
 using System;
 using System.Collections.Generic;
 
-public class List
+class List
 {
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
         List<int> result = new List<int>();
-        for (int i = 0; i < listLength; i++)
+
+        try
         {
-            try
-            {
-                if (i >= list1.Count || i >= list2.Count)
-                {
-                    throw new ArgumentOutOfRangeException();
-                }
-                if (list2[i] == 0)
-                {
-                    throw new DivideByZeroException();
-                }
-                result.Add(list1[i] / list2[i]);
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Cannot divide by zero");
-                result.Add(0);
-            }
-            catch (ArgumentOutOfRangeException)
+            if (list1.Count < listLength || list2.Count < listLength)
             {
                 Console.WriteLine("Out of range");
-                result.Add(0);
+                return result;
+            }
+
+            for (int i = 0; i < listLength; i++)
+            {
+                try
+                {
+                    if (list2[i] == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                        result.Add(0);
+                    }
+                    else
+                    {
+                        result.Add(list1[i] / list2[i]);
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Out of range");
+                    break;
+                }
             }
         }
+        catch (Exception)
+        {
+            // Handle other unexpected errors.
+            Console.WriteLine("An error occurred");
+        }
+
         return result;
     }
 }
