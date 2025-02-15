32c946b8498201f7e5fea30647e3cb48576743c9
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-02-15 18:07:20 +0200

    Update 0-main.cs

--- csharp-classes/0-enemy/0-main.cs
+++ csharp-classes/0-enemy/0-main.cs
@@ -6,7 +6,9 @@ class Program
     static void Main(string[] args)
     {
         Zombie newObject = new Zombie();
-
+         /// <summary>
+    /// Define obeject here.
+    /// </summary>
         Console.WriteLine("newObject is of type {0}", newObject.GetType());
         Console.WriteLine("String representation: {0}", newObject.ToString());
     }
