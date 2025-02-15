99d957fd1f5724142f19f5f138f3eb176b0574fa
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-02-15 17:55:43 +0200

    Update 0-enemy.cs

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -1,7 +1,11 @@
-﻿namespace Enemies
+using System;
+
+namespace Enemies
 {
+    /// <summary>
+    /// Represents a zombie enemy.
+    /// </summary>
     public class Zombie
     {
-        // Empty class definition for Zombie
     }
 }
