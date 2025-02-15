63bea2cb063219d852b2805ec6eb85b5cd6e1eaf
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-02-15 18:20:29 +0200

    Update 4-main.cs

--- csharp-classes/4-enemy/4-main.cs
+++ csharp-classes/4-enemy/4-main.cs
@@ -1,8 +1,14 @@
 using Enemies;
 using System;
 
+/// <summary>
+/// Entry point of the program.
+/// </summary>
 class Program
 {
+    /// <summary>
+    /// Main method that creates Zombie objects and displays their type and health.
+    /// </summary>
     static void Main(string[] args)
     {
         Enemies.Zombie newObject1;
@@ -23,6 +29,7 @@ class Program
         {
             newObject2 = new Enemies.Zombie(98);
             Console.WriteLine("{0} is of type {1} and has a total of {2} health", newObject2.Name, newObject2.GetType(), newObject2.GetHealth());
+
         }
         catch (Exception e)
         {
