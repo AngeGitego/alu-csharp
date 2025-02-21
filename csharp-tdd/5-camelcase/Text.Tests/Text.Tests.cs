89e988828a80f47ff8720640575c41a4c0bd2af7
AngeGitego <k.gitego@alustudent.com>
2025-02-21 21:18:45 +0200

    Initial commit for 5-camelcase task

---
+++ csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Text;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class StrTests
+    {
+        [Test]
+        public void CamelCase_EmptyString_ReturnsZero()
+        {
+            var result = Str.CamelCase("");
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void CamelCase_SingleWord_ReturnsOne()
+        {
+            var result = Str.CamelCase("word");
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void CamelCase_MultipleWords_ReturnsCorrectCount()
+        {
+            var result = Str.CamelCase("camelCaseStringTest");
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void CamelCase_WithEmptyWords_ReturnsCorrectCount()
+        {
+            var result = Str.CamelCase("oneCamelCaseWord");
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,24 @@
+﻿namespace Text
+{
+    public class Str
+    {
+        /// <summary>
+        /// Determines how many words are in a camelCase string.
+        /// </summary>
+        /// <param name="s">The camelCase string.</param>
+        /// <returns>The number of words in the string.</returns>
+        public static int CamelCase(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            int wordCount = 1; // The first word is always counted
+            foreach (char c in s)
+            {
+                if (char.IsUpper(c))
+                    wordCount++;
+            }
+            return wordCount;
+        }
+    }
+}
