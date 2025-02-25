c88841deffc8395b94a85bec90cde29d23736bf9
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-02-25 23:02:24 +0200

    Update 12-fizzbuzz.cs

--- csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -2,22 +2,19 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         for (int i = 1; i <= 100; i++)
         {
             if (i % 3 == 0 && i % 5 == 0)
-                Console.Write("FizzBuzz");
+                Console.Write("FizzBuzz ");
             else if (i % 3 == 0)
-                Console.Write("Fizz");
+                Console.Write("Fizz ");
             else if (i % 5 == 0)
-                Console.Write("Buzz");
+                Console.Write("Buzz ");
             else
-                Console.Write(i);
-
-            if (i < 100)
-                Console.Write(" ");
+                Console.Write(i + " ");
         }
-        Console.WriteLine(); // Ensures a final newline is printed
+        Console.WriteLine(); // New line at the end
     }
 }
