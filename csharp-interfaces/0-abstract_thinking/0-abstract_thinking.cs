bc9aa768f91665e1a04419515f95b8fa1e65e455
AngeGitego <k.gitego@alustudent.com>
2025-06-18 16:45:33 +0300

    new

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Abstract class Base containing a name property and an overridden ToString() method.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public property to store the name.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Overrides the default ToString() method to return "<name> is a <type>"
+    /// </summary>
+    /// <returns>A formatted string.</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
